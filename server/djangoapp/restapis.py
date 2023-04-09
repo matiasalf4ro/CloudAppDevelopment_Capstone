338bb14f113c868c92453a26d8abaa441638e7ad
matiasalf4ro <matiassnts@gmail.com>
2023-04-09 02:40:32 -0400

    Update 2.0

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,7 +3,6 @@ import json
 import os
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
-from decouple import config
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
@@ -45,7 +44,7 @@ def post_request(url, json_payload, **kwargs):
 def get_dealers_from_cf(url):
     results = []
     json_result = get_request(url)
-    dealers = json_result["body"]["rows"]
+    dealers = json_result
 
     for dealer in dealers:
 
@@ -54,7 +53,7 @@ def get_dealers_from_cf(url):
         dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                short_name=dealer_doc["short_name"],
-                               st=dealer_doc["st"], state=dealer_doc["state"], zip=dealer_doc["zip"])
+                               st=dealer_doc["st"], zip=dealer_doc["zip"])
         results.append(dealer_obj)
 
     return results
@@ -63,14 +62,13 @@ def get_dealer_by_id(url, dealer_id):
 
     json_result = get_request(url, dealerId=dealer_id)
 
-    dealer = json_result["entries"][0]
-    dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
-                           id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
-                           short_name=dealer["short_name"],
-                           st=dealer["st"], state=dealer["state"], zip=dealer["zip"])
+    dealer = json_result
+    dealer_obj = CarDealer(address=dealer, city=dealer, full_name=dealer,
+                           id=dealer, lat=dealer, long=dealer,
+                           short_name=dealer,
+                           st=dealer, zip=dealer)
 
     return dealer_obj
-
 
 def get_dealers_by_state(url, state):
     results = []
@@ -88,7 +86,6 @@ def get_dealers_by_state(url, state):
 
     return results
 
-
 def get_dealer_reviews_from_cf(url, dealer_id):
     results = []
 
@@ -96,22 +93,22 @@ def get_dealer_reviews_from_cf(url, dealer_id):
 
     if json_result:
 
-        reviews = json_result["body"]["data"]["docs"]
+        reviews = json_result
 
         for review in reviews:
 
-            review_content = review["review"]
-            id = review["_id"]
-            name = review["name"]
-            purchase = review["purchase"]
-            dealership = review["dealership"]
+            review_content = review
+            id = review
+            name = review
+            purchase = review
+            dealership = review
 
             try:
      
-                car_make = review["car_make"]
-                car_model = review["car_model"]
-                car_year = review["car_year"]
-                purchase_date = review["purchase_date"]
+                car_make = review
+                car_model = review
+                car_year = review
+                purchase_date = review
 
                 review_obj = DealerReview(dealership=dealership, id=id, name=name, 
                                           purchase=purchase, review=review_content, car_make=car_make, 
@@ -132,9 +129,33 @@ def get_dealer_reviews_from_cf(url, dealer_id):
     return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(review_text):
+    try:
+        if os.environ['env_type'] == 'PRODUCTION':
+            url = os.environ['https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/70bac3e1-be9e-4377-ad7b-65b95373e5ac']
+            api_key = os.environ["iXIXC9JqfDqvXHI5IeDG_L7tuPR62EmJXxjYQtINqw88"]
+    except KeyError:
+        url = 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/70bac3e1-be9e-4377-ad7b-65b95373e5ac'
+        api_key = 'iXIXC9JqfDqvXHI5IeDG_L7tuPR62EmJXxjYQtINqw88'
+
+    version = '2022-03-30'
+    authenticator = IAMAuthenticator(api_key)
+    nlu = NaturalLanguageUnderstandingV1(
+        version=version, authenticator=authenticator)
+    nlu.set_service_url(url)
+
+    try:
+        response = nlu.analyze(text=review_text, features=Features(
+            sentiment=SentimentOptions())).get_result()
+        print(json.dumps(response))
+        sentiment_label = response
+    except:
+        print("Review is too short for sentiment analysis.")
+        sentiment_label = "neutral"
+
+    print(sentiment_label)
+
+    return sentiment_label
 
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -74,49 +74,17 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
-def get_request(url, **kwargs):
-    print(kwargs)
-    print("GET from {} ".format(url))
-    try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
-    except:
-        # If any error occurs
-        print("Network exception occurred")
-    status_code = response.status_code
-    print("With status {} ".format(status_code))
-    json_data = json.loads(response.text)
-    return json_data
-
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/e5daf4d1-fa37-4675-a342-40f7c05a522d/dealership-package/get-dealership"
-        
+        # Get dealers from the Cloudant DB
         context["dealerships"] = get_dealers_from_cf(url)
+
+        # dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
+        # return HttpResponse(dealer_names)
+
         return render(request, 'djangoapp/index.html', context)
-
-def get_dealers_from_cf(url, **kwargs):
-    results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url)
-    if json_result:
-        # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
-        # For each dealer object
-        for dealer in dealers:
-            # Get its content in `doc` object
-            dealer_doc = dealer["doc"]
-            # Create a CarDealer object with values in `doc` object
-            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
-                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   short_name=dealer_doc["short_name"],
-                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
-            results.append(dealer_obj)
-
-    return results
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
